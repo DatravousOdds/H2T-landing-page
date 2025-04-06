b86ab11bc78999953d9f759c2d6c5b5042452ba5
Datravous Odds <datravousodds@gmail.com>
2025-04-06 14:12:23 -0500

    feat: implement Twilio SMS notification function and update Firebase configuration
    - Added a new Cloud Function to send waitlist notifications via Twilio SMS.
    - Initialized Twilio client with environment variables for account credentials.
    - Updated Firebase functions imports in index.html for better integration.
    - Removed deprecated Twilio client initialization from index.html.

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,39 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const twilio = require("twilio");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+admin.initializeApp();
+
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const notificationPhoneNumber = process.env.NOTIFICATION_PHONE_NUMBER;
+
+// Initialize Twilio Client
+const twilioClient = twilio(twilioAccountSid, twilioAuthToken);
+
+exports.sendWaitlistNotification = functions.https.onCall(
+  async (data, context) => {
+    try {
+      const email = data.email;
+
+      if (!email) {
+        throw new Error("No email provided");
+      }
+    } catch (error) {
+      console.log("Error when trying to send SMS message");
+    }
+  }
+);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
